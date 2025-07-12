62d82a088f985ac7480be2c28477120bc890dfeb
Swati Verma <vswati535@gmail.com>
2025-07-12 19:24:26 +0530

    Initial Commit

---
+++ test.sh
@@ -0,0 +1,8 @@
+# test.sh
+
+#!/bin/bash
+echo "Hey Buddy"
+touch /tmp/script/testscriptfile # give the location here otherwise script will run in root directory
+
+# command to give execute permission to the file
+# chmod u+x test.sh
